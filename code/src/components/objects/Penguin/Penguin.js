cf50e22a4337f46a22f51e9603ba40fbc91dfbd3
Jonah Lytle <jlytle@princeton.edu>
2020-05-01 20:50:23 -0400

    new

--- code/src/components/objects/Penguin/Penguin.js
+++ code/src/components/objects/Penguin/Penguin.js
@@ -9,12 +9,12 @@ class Penguin extends Group {
         super();
 
         // Init state
-        this.state = {
-            gui: parent.state.gui,
-            bob: true,
-            spin: this.spin.bind(this),
-            twirl: 0,
-        };
+        //this.state = {
+            //gui: parent.state.gui,
+            //bob: true,
+            //spin: this.spin.bind(this),
+            //twirl: 0,
+        //};
 
         // Load object
         const loader = new GLTFLoader();
@@ -25,48 +25,48 @@ class Penguin extends Group {
         });
 
         // Add self to parent's update list
-        parent.addToUpdateList(this);
+        //parent.addToUpdateList(this);
 
         // Populate GUI
-        this.state.gui.add(this.state, 'bob');
-        this.state.gui.add(this.state, 'spin');
+        //this.state.gui.add(this.state, 'bob');
+        //this.state.gui.add(this.state, 'spin');
     }
 
-    spin() {
+    //spin() {
         // Add a simple twirl
-        this.state.twirl += 6 * Math.PI;
+        //this.state.twirl += 6 * Math.PI;
 
         // Use timing library for more precice "bounce" animation
         // TweenJS guide: http://learningthreejs.com/blog/2011/08/17/tweenjs-for-smooth-animation/
         // Possible easings: http://sole.github.io/tween.js/examples/03_graphs.html
-        const jumpUp = new TWEEN.Tween(this.position)
-            .to({ y: this.position.y + 1 }, 300)
-            .easing(TWEEN.Easing.Quadratic.Out);
-        const fallDown = new TWEEN.Tween(this.position)
-            .to({ y: 0 }, 300)
-            .easing(TWEEN.Easing.Quadratic.In);
+        //const jumpUp = new TWEEN.Tween(this.position)
+            //.to({ y: this.position.y + 1 }, 300)
+          //  .easing(TWEEN.Easing.Quadratic.Out);
+        //const fallDown = new TWEEN.Tween(this.position)
+          //  .to({ y: 0 }, 300)
+          //  .easing(TWEEN.Easing.Quadratic.In);
 
         // Fall down after jumping up
-        jumpUp.onComplete(() => fallDown.start());
+        //jumpUp.onComplete(() => fallDown.start());
 
         // Start animation
-        jumpUp.start();
-    }
+      //  jumpUp.start();
+    //}
 
-    update(timeStamp) {
-        if (this.state.bob) {
+  //  update(timeStamp) {
+    //    if (this.state.bob) {
             // Bob back and forth
-            this.rotation.z = 0.05 * Math.sin(timeStamp / 300);
-        }
-        if (this.state.twirl > 0) {
+      //      this.rotation.z = 0.05 * Math.sin(timeStamp / 300);
+      //  }
+      //  if (this.state.twirl > 0) {
             // Lazy implementation of twirl
-            this.state.twirl -= Math.PI / 8;
-            this.rotation.y += Math.PI / 8;
-        }
+        //    this.state.twirl -= Math.PI / 8;
+          //  this.rotation.y += Math.PI / 8;
+        //}
 
         // Advance tween animations, if any exist
-        TWEEN.update();
-    }
+        //TWEEN.update();
+    //}
 }
 
 export default Penguin;
