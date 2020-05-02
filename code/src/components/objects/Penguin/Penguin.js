798d237b1a5ab48093a47de0f86829525056ceda
Jonah Lytle <jlytle@princeton.edu>
2020-05-02 10:21:37 -0400

    new

--- code/src/components/objects/Penguin/Penguin.js
+++ code/src/components/objects/Penguin/Penguin.js
@@ -1,7 +1,7 @@
 import { Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
-import MODEL from './scene.gltf';
+import MODEL from './penguin.gltf';
 
 class Penguin extends Group {
     constructor(parent) {
