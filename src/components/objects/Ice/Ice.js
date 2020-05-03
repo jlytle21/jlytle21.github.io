4fc8b8f8114c90ee671e067f79ac31b5a1fcb9d6
Rich Wolf <rwolf@princeton.edu>
2020-05-03 11:18:21 -0400

    Changed back to original land

--- src/components/objects/Ice/Ice.js
+++ src/components/objects/Ice/Ice.js
@@ -1,6 +1,6 @@
 import { Group, Vector3 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import MODEL from './model.obj';
+import MODEL from './land.gltf';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
 class Ice extends Group {
@@ -8,20 +8,22 @@ class Ice extends Group {
         // Call parent Group() constructor
         super();
 
-        const loader = new OBJLoader();
+        const loader = new GLTFLoader();
 
         this.name = 'ice';
 
         loader.load(MODEL, (object) => {
             console.log(object);
-            let offset = new Vector3(0.0, -3.25, 0.0);
-            object.position.add(offset);
+            let offset = new Vector3(0.0, -1, 0.0);
+            object.scene.scale.multiplyScalar(20);
+            object.scene.position.add(offset);
+            //object.position.add(offset);
 
-            object.rotation.set(0.0,0.0,0.0);
-            object.scale.multiplyScalar(80);
-            object.rotateX(-75*Math.PI/180);
-            object.rotateY(-10*Math.PI/180);
-            this.add(object);
+            //object.rotation.set(0.0,0.0,0.0);
+            //object.scale.multiplyScalar(80);
+            //object.rotateX(-75*Math.PI/180);
+            //object.rotateY(-10*Math.PI/180);
+            this.add(object.scene);
         });
     }
 }
