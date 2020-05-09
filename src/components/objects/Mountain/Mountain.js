09fba551fd07f217c917fc647b292545f0df41e0
Jonah Lytle <jlytle@princeton.edu>
2020-05-09 16:54:33 -0400

    new

--- src/components/objects/Mountain/Mountain.js
+++ src/components/objects/Mountain/Mountain.js
@@ -1,32 +1,33 @@
-import { Group, Vector3 } from 'three';
-import MODEL from './m.obj';
-import MATERIAL from './m.mtl';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { Group, Vector3, TextureLoader } from 'three';
+import MODEL from './mount.obj';
+import MATERIAL from './albedo.png';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
 class Mountain extends Group {
   constructor() {
-    // from https://poly.google.com/view/0GzMb32v3oH
+    // from https://sketchfab.com/3d-models/praise-the-sun-737e12518b1b4e3da8e4406d7da83b90
     super();
 
-    const mtlloader = new MTLLoader();
+    let texture = new TextureLoader().load(MATERIAL);
 
     this.name = 'mountain';
 
-    mtlloader.load(MATERIAL, (materials) => {
-      materials.preload();
-      const loader = new OBJLoader();
-      loader.setMaterials(materials);
+    const loader = new OBJLoader();
 
-      loader.load(MODEL, (object) => {
-        object.scale.multiplyScalar(3);
-        object.position.x = -1000;
-        object.position.z = -1000;
-        object.position.y -= 10;
-        object.rotation.y = 1;
-        this.add(object);
+    loader.load(MODEL, (object) => {
+      object.traverse((child) => {
+        if (child.type == "Mesh") {
+          child.material.map = texture;
+        }
       });
+      object.scale.multiplyScalar(1000);
+      object.position.y -= 12;
+      object.position.x = -1000;
+      object.position.z = -1000;
+
+      this.add(object);
     });
+
   }
 }
 
